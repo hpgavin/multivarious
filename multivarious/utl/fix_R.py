81d47d55645ce434da2bcf949a69ebb7b2ced1ec
Henri P Gavin <henri.gavin@duke.edu>
2026-02-03 09:48:22 -0500

    closing in on SQP to fix R

--- multivarious/utl/fix_R.py
+++ multivarious/utl/fix_R.py
@@ -1,68 +1,69 @@
 import numpy as np
 from multivarious.opt import sqp
-from multivarious.utl import format_plot
 from multivarious.utl import plot_cvg_hst
+from multivarious.utl import format_plot
 
 def fit_R(v, C):
 
+    # extract constants from the SimpleNamespace C
     n = C.n
-    upper_tri_indices = C.idx
+    upper_tri_idx = C.idx
     Ro = C.Ro
-    eVal_min = C.eVal
+    eVal_lb = C.eVal_lb
 
     R = np.eye((n, n)) # Initialize an n x n identity matrix 
 
-    # Place the values into the upper triangle
-    R[upper_tri_indices] = v
+    # Place the values v into the upper triangle
+    R[upper_tri_idx] = v
 
     # Mirror the upper triangle to the lower triangle to make symmetric
-    R[(upper_tri_indices[1], upper_tri_indices[0])] = v
+    R[(upper_tri_idx[1], upper_tri_idx[0])] = v
 
-    eVal_0 = np.min ( np.eigh(R) )
+    eVal, eVec = np.linalg.eigh(R) 
 
-    f = np.trace( (R - Ro)@(R-Ro) ) # Frobeneous norm trace ??
+    f = np.trace( (R - Ro) @ (R - Ro) ) # Frobeneous norm squared. 
 
-    g = np.array([ eVal_min - eVal_0 ]) # ??
+    g = np.array([ eVal_lb - eVal[0] ]) 
 
     return f, g
 
-def fix_R(Ro, eVal_min):
+def fix_R(Ro, eVal_lb):
     '''
     fix_R
 
-    min || Ro - R(v) ||_F^2
+    min || R(v) - Ro ||_F^2
 
-    v is the upper triangle (and lower triangle) of R
+    where v is the upper triangle (and lower triangle) of (symm) R
 
-    s.t. -1 < v < 1  and  0 < eVal_min < min(eVal(R(v)))
+    s.t. -1 < v < 1  and  0 < eVal_lb < min(eVal(R(v)))
 
     INPUTS:
-      Ro = initial guess for the correlation matrix
-      eVal_min ... lowest acceptable eigenvalue of R
+      Ro = initial guess for the correlation matrix (2D array)
+      eVal_lb = lower bound on eigenvalue of R (int)
  
     OUTPUT:
-      R = the fixed correlation matrix
+      R = the fixed correlation matrix (2D array)
 
     # Transformation matrix
     T = eVec @ np.diag(np.sqrt(eVal))
     '''
 
-    C = SimpleNamespace() # Constants used within the optimization analysis 
-
     n = Ro.shape[0]
    
     # indices of the upper triangle (excluding the diagonal)
-    upper_tri_indices = np.triu_indices(n, k=1)
+    upper_tri_idx = np.triu_indices(n, k=1)
 
-    # upper triangle elements into a 1D array
-    v_init = Ro[upper_tri_indices]
-    v_ub =  np.ones( n*(n-1)/2 )
-    v_lb = -np.ones( n*(n-1)/2 )
-
+    # Constants used within the optimization analysis 
+    C = SimpleNamespace()
     C.n = n
     C.Ro = Ro
-    C.idx = uppoer_triangle_indices
-    C.eVal = eVal_min
+    C.idx = upper_tri_idx
+    C.eVal_lb = eVal_lb
+
+    # upper triangle elements into a 1D array
+    v_init = Ro[upper_tri_idx]
+    v_ub =  np.ones( n*(n-1)/2 )
+    v_lb = -np.ones( n*(n-1)/2 )
 
     # optimization options ...
     #        0     1       2       3        4         5     6     7    8
@@ -73,13 +74,13 @@ def fix_R(Ro, eVal_min):
     v_opt, f_opt, g_opt, cvg_hst, _,_ = sqp(fit_R, v_init, v_lb, v_ub, opts, C)
 
     # Build the correlation matrix from v_opt
-    R = np.eye((n, n)) # Initialize an n x n identity matrix 
+    R = np.eye( n, n ) # Initialize an n x n identity matrix 
 
     # Place the values into the upper triangle
-    R[upper_tri_indices] = v_opt
+    R[upper_tri_idx] = v_opt
 
     # Mirror the upper triangle to the lower triangle to make symmetric
-    R[(upper_tri_indices[1], upper_tri_indices[0])] = v_opt
+    R[(upper_tri_idx[1], upper_tri_idx[0])] = v_opt
 
     # plot the convergence history
     format_plot(font_size=15, line_width=3, marker_size=7)
