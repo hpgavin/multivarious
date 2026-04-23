35433246719b0b2fccc146485dbddbb45c68a5be
Henri P Gavin <henri.gavin@duke.edu>
2026-04-23 06:16:52 -0400

    opt_example^Core.py -> opt_example.py

--- examples/opt_example.py
+++ examples/opt_example.py
@@ -1,9 +1,9 @@
-#! /usr/bin/env -S python3 -i 
+#! /usr/bin/env -S /usr/bin/python3 -i 
 
 import numpy as np
-from rich.traceback import install; install()
 from multivarious.opt import ors, nms, sqp
-from multivarious.utl import StableNamespace, format_plot, plot_cvg_hst
+from multivarious.utl import StableNamespace, plot_cvg_hst, format_plot 
+from rich.traceback import install; install()
 
 # Define the optimization problem. =========================================
 def opt_example_analysis( v, cts ):
@@ -13,10 +13,10 @@ def opt_example_analysis( v, cts ):
 
     Parameters
     ----------
-    v   : array-like (n,)
-          design varialbes (in original units, not scaled) 
+    v : array-like (n,)
+        design varialbes (in original units, not scaled) 
     cts : any
-          collection of constants needed for the analysis 
+        collection of constants needed for the analysis 
 
     Returns
     -------
@@ -25,7 +25,6 @@ def opt_example_analysis( v, cts ):
     g : arry-like (m,)
         design constraints
     """
-
     v1 = v[0]                   # description of design variable "v1", units
     v2 = v[1]                   # description of design variable "v2", units
 
@@ -42,34 +41,82 @@ def opt_example_analysis( v, cts ):
        b[0] + b[3]*( v1 - b[1] )**2 + b[4]*( v2 - b[2] )**2    # "g2"
     ])
 
-    return f[0], g              # end of opt_example_analysis()
+    return f, g       
+
+def opt_example_analysis_HW01P10( v, cts ):
+    """
+    HW 1 , P 10
+    """
+
+    v1 = v[0]                   # description of design variable "v1", units
+    v2 = v[1]                   # description of design variable "v2", units
+
+    c = cts.c                   # description of constant c, units
+
+    f = 2 + v1/c[0] + v2/c[1] + cos(v1*v2/c[2])  # HW 1, P 10
+
+    g = np.array([ -1 ]);                        # HW 1, P 10 , no constraint
+
+    return f, g       
 
 # Set-up and Solve the optimization problem. ===============================
 
-# Constants used in the design objective and the design constraint functions
-# attributes of cts may be lists, nparrays, text ... anything
+# Three examples to choose from:
+#    example A: open constraints
+#    example B: gauntlet constraints
+#    example C: multiple minima
+
+example = 'A' 
+
+# example A is the default example 
 cts = StableNamespace(
-    a = [ -0.4,  0.2,  0.5,  1.4,  1.4 ] , 
-    b = [  1.0, -0.5,  0.5, -1.4, -1.4 ] , 
-    c = [  0.0,  0.8,  0.2 ] , 
-)
+    a = [ -0.4,  0.2,  0.5,  1.4,  1.4 ], # open constraints
+    b = [  1.0, -0.5,  0.5, -1.4, -1.4 ],
+    c = [  0.0,  0.8,  0.2 ] )
 
-v_lb = np.array([ 0.0,  0.0])       # lower bound on the design variables 
-v_ub = np.array([ 1.0,  1.0])       # upper bound on the design variables 
+fctn = opt_example_analysis
 
-n = len(v_lb)                       # the number of design variables 
+v_lb = np.array([ 0.0,  0.0])    # lower bound on the design variables 
+v_ub = np.array([ 1.0,  1.0])    # upper bound on the design variables 
 
-#v_init = v_lb + np.random.rand(n)*(v_ub - v_lb)   # a  random  initial guess
-v_init = np.array([ 0.8 , 0.8 ])                   # a specific initial guess 
+v_init = np.array([ 0.8 , 0.8 ]) # a specific initial guess 
+
+if example == 'B': # gauntlet constraints
+    fctn = opt_example_analysis
+    cts = StableNamespace(
+        a = [  0.7,  1.25,  0.5, -1.0, -1.0 ], # gauntlet constraints
+        b = [  1.0, -0.5,   0.5, -1.4, -1.4 ],
+        c = [  0.0,  0.8,   0.2 ] )
+
+    v_lb = np.array([ 0.0,  0.0])    # lower bound on the design variables 
+    v_ub = np.array([ 1.0,  1.0])    # upper bound on the design variables 
+
+    v_init = np.array([ 0.3 , 0.9 ]) # a specific initial guess 
+
+if example == 'C': # multiple minima
+    fctn = opt_example_analysis_HW01P10
+    cts = StableNamespace(
+        c = [ 40, 30, 20 ] )
+    v_lb = np.array([ -10.0, -10.0]) # lower bound on the design variables HW 1, P10
+    v_ub = np.array([  10.0,  10.0]) # upper bound on the design variables HW 1, P10
+
+    v_init = np.array([ 0.8, 0.4 ]) # a specific initial guess 
+
+n = len(v_lb) # the number of design variables 
+
+#v_init = v_lb + np.random.rand(n)*(v_ub - v_lb) # a random initial guess
+v_init = v_init + 0.10*np.random.rand(n)*(v_ub - v_lb) # a random initial guess
 
 # optimization hyperparameters ...
-#       0     1       2       3        4         5     6     7    8
-#      msg   tol_v   tol_f   tol_g  max_evals  pnlty expn  m_max cov_F
-hyp = [ 3,   5e-2,   5e-2,   1e-3,    50*n**3,  0.7,  0.5,   1,  0.05 ]
+#       0     1       2      3       4         5      6     7      8
+#      msg   tol_v   tol_f  tol_g  max_evals  pnlty  expn  m_max  cov_F
+hyp = [ 3,   2e-2,   2e-2,  1e-3,   50*n**3,  0.7,   0.5,   1,    0.05 ]
 
 # Solve the optimization problem using one of ... ors , nms , sqp 
-v_opt, f_opt, g_opt, cvg_hst, _,_ = nms( opt_example_analysis, v_init, v_lb, v_ub, hyp, cts )
+# in opt_example_analysis select the "open constraint" or "gauntlet constraint"
+v_opt, f_opt, g_opt, cvg_hst, _,_ = nms( fctn, v_init, v_lb, v_ub, hyp, cts )
 
 # plot the convergence history
 format_plot(font_size=15, line_width=3, marker_size=7)
-plot_cvg_hst( cvg_hst , v_opt, hyp, save_plots=True ) 
+plot_cvg_hst( cvg_hst , v_opt, hyp, save_plots = True ) 
+
